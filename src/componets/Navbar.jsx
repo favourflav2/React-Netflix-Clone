fdb00bba3fe135347fc4b921bb83ea03cde3483e
favourflav <chukwagon1@gmail.com>
2022-12-26 08:38:09 -0800

    edit

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -18,20 +18,20 @@ export default function Navbar() {
   }
   return (
     <div className='flex items-center justify-between p-4 z-[100] w-full absolute' >
-        <Link to='/React-Netflix-Clone/'>
+        <Link to='/'>
         <h1 className='text-red-600 text-4xl font-bold cursor-auto'>NETFLIX</h1> 
         </Link>
 
       {user?.email ? 
       <div>
-      <Link to='/React-Netflix-Clone/account'><button className='text-white pr-4'>Account</button></Link>
+      <Link to='/account'><button className='text-white pr-4'>Account</button></Link>
       <button onClick={handleLogOut} className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white'>Sign Out</button>
       
     </div>
       : 
       <div>
-        <Link to='/React-Netflix-Clone/login'><button className='text-white pr-4'>Sign In</button></Link>
-        <Link to='/React-Netflix-Clone/signup'><button className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white'>Sign Up</button></Link>
+        <Link to='/login'><button className='text-white pr-4'>Sign In</button></Link>
+        <Link to='/signup'><button className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white'>Sign Up</button></Link>
         
       </div>
       }
--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { MainContext } from './context/MainContext';
 import AuthContext from './context/AuthContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<BrowserRouter>
+<HashRouter>
   <AuthContext>
     <MainContext>
       <App />
     </MainContext>
   </AuthContext>
-</BrowserRouter>
+</HashRouter>
 );
 
 
